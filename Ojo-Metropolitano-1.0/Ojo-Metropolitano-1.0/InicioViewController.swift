9d7605e66e8e9d17611c2f7464535b00203482b9
MrJoyner95 <oct_romo@hotmail.com>
2017-09-04 20:35:25 -0500

    Login funcional, Inicio mezclado

--- Ojo-Metropolitano-1.0/Ojo-Metropolitano-1.0/InicioViewController.swift
+++ Ojo-Metropolitano-1.0/Ojo-Metropolitano-1.0/InicioViewController.swift
@@ -7,13 +7,38 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class InicioViewController: UIViewController {
-
+class InicioViewController: UIViewController, CLLocationManagerDelegate{
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        let span: MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        let userLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        
+        let region: MKCoordinateRegion = MKCoordinateRegionMake(userLocation, span)
+        
+        mapView.setRegion(region, animated: true)
+        
+        self.mapView.showsUserLocation = true
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
 
-        // Do any additional setup after loading the view.
+        //mapView.setRegion(MKCoordinateRegionMakeWithDistance(CLLocationCoordinate2D (latitude: 20.695258, longitude: -103.348564),1500, 1500 ),animated: true)
     }
 
     override func didReceiveMemoryWarning() {
